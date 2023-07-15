b408510c162eb932e88efe91fc3c93cb087b5f9c
dukim <sajacall82@naver.com>
2023-07-15 09:56:57 +0900

    update 08

---
+++ docs/08_딥러닝을이용한투자전략/core/cnn.py
@@ -0,0 +1,138 @@
+from tensorflow.python import keras
+from tensorflow.python.keras.layers.core import Dropout
+from core.utils import read_config, build_dataset
+
+
+class cnn:
+    def __init__(self, config_path):
+        config = read_config(config_path)
+        self.input_dir = config['cnn']['dataset']
+        self.img_size = int(config['base']['img_size'])
+        self.channel = int(config['base']['img_channel'])
+        self.epochs = int(config['cnn']['epochs'])
+        self.batch_size = int(config['cnn']['batch_size'])
+        self.output = config['cnn']['model_name']
+
+    def build_model(self, shape, seed=None):
+        import numpy as np
+        from keras import layers
+        import tensorflow as tf
+
+        if seed:
+            np.random.seed(seed)
+
+        model = tf.keras.models.Sequential()
+        model.add(layers.Conv2D(32,
+                                (3, 3),
+                                activation='relu',
+                                padding='same',
+                                kernel_initializer='glorot_uniform',
+                                input_shape=(shape)))
+        model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(layers.Conv2D(48,
+                                (3, 3),
+                                activation='relu',
+                                padding='same',
+                                kernel_initializer='glorot_uniform'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(layers.Conv2D(64,
+                                (3, 3),
+                                activation='relu',
+                                padding='same',
+                                kernel_initializer='glorot_uniform'))
+        model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(layers.Conv2D(96,
+                                (3, 3),
+                                activation='relu',
+                                padding='same',
+                                kernel_initializer='glorot_uniform'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(layers.Flatten())
+        model.add(layers.Dense(256, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(layers.Dense(2, activation='softmax'))
+
+        return model
+
+    def run_model(self):
+        import math
+        from keras.optimizers import Adam
+        import numpy as np
+        from sklearn.metrics import confusion_matrix
+        from sklearn.metrics import classification_report
+        from datetime import datetime
+
+        now = datetime.now()
+
+        shape = (self.img_size, self.img_size, self.channel)
+        model = self.build_model(shape)
+        print(model.summary())
+        model.compile(
+            optimizer=Adam(learning_rate=1.0e-4),
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+
+        x_train, y_train, nb_classes = build_dataset(
+            "{}/train".format(self.input_dir),
+            self.img_size)
+        x_test, y_test, nb_classes = build_dataset(
+            "{}/test".format(self.input_dir),
+            self.img_size)
+
+        model.fit(
+            x_train,
+            y_train,
+            batch_size=self.batch_size,
+            epochs=self.epochs)
+
+        print("model 예측")
+        predicted = model.predict(x_test)
+        y_pred = np.argmax(predicted, axis=1)
+        y_test = np.argmax(y_test, axis=1)
+        cm = confusion_matrix(y_test, y_pred)
+        report = classification_report(y_test, y_pred)
+        tn = cm[0][0]
+        fn = cm[1][0]
+        tp = cm[1][1]
+        fp = cm[0][1]
+        if tp == 0:
+            tp = 1
+        if tn == 0:
+            tn = 1
+        if fp == 0:
+            fp = 1
+        if fn == 0:
+            fn = 1
+        TPR = float(tp)/(float(tp)+float(fn))
+        FPR = float(fp)/(float(fp)+float(tn))
+        accuracy = round((float(tp) + float(tn))/(float(tp) +
+                                                  float(fp) + float(fn) + float(tn)), 3)
+        specitivity = round(float(tn)/(float(tn) + float(fp)), 3)
+        sensitivity = round(float(tp)/(float(tp) + float(fn)), 3)
+        mcc = round((float(tp)*float(tn) - float(fp)*float(fn))/math.sqrt(
+            (float(tp)+float(fp))
+            * (float(tp)+float(fn))
+            * (float(tn)+float(fp))
+            * (float(tn)+float(fn))
+        ), 3)
+
+        print(now.strftime("%d/%m/%Y %H:%M:%S"))
+        print('{}epochs_{}batch_cnn'.format(self.epochs, self.batch_size))
+        print('TN: {}'.format(tn))
+        print('FN: {}'.format(fn))
+        print('TP: {}'.format(tp))
+        print('FP: {}'.format(fp))
+        print('TPR: {}'.format(TPR))
+        print('FPR: {}'.format(FPR))
+        print('accuracy: {}'.format(accuracy))
+        print('specitivity: {}'.format(specitivity))
+        print("sensitivity : {}".format(sensitivity))
+        print('mcc : {}'.format(mcc))
+        print('{}'.format(report))
